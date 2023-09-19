e62535b0be610fcb12475ba79dd881e1c3a789b9
Biel009xs <107139661+Biel009xs@users.noreply.github.com>
2023-09-19 13:22:57 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,4 +4,16 @@ function tocaSomPom (idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
+//para
+ for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
+   
+   const tecla = ListaDeTeclas[contador];
+   const instrumento = tecla.classList[1];
+   const idAudio = `#som_${instrumento}`; //template string
+
+   tecla.onclick = function () {
+     tocaSom(idAudio);
+   }
+
+ }
 ListaDeTeclas[0].onclick = tocaSomPom;
